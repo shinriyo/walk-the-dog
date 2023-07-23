028ecbcc2b0b48ad6f6534abf70802fa8a4906c9
shinriyo <shinriyo@gmail.com>
2023-07-23 16:41:55 +0900

    sirpinski

--- src/lib.rs
+++ src/lib.rs
@@ -48,12 +48,15 @@ pub fn main_js() -> Result<(), JsValue> {
     context.fill();
     */
 
+    /*
     draw_triangle(&context, [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)]);
 
     draw_triangle(&context, [(300.0, 0.0), (150.0, 300.0), (450.0, 300.0)]);
     draw_triangle(&context, [(150.0, 300.0), (0.0, 600.0), (300.0, 600.0)]);
     draw_triangle(&context, [(450.0, 300.0), (300.0, 600.0), (600.0, 600.0)]);
+    */
 
+    sirpinski(&context, [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)], 2);
     Ok(())
 }
 
@@ -66,4 +69,12 @@ fn draw_triangle(context: &web_sys::CanvasRenderingContext2d, points: [(f64, f64
     context.line_to(top.0, top.1);
     context.close_path();
     context.stroke()
+}
+
+fn sirpinski(context: &web_sys::CanvasRenderingContext2d,
+  points: [(f64, f64); 3], depth: u8) {
+    draw_triangle(&context, [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)]);
+    draw_triangle(&context, [(300.0, 0.0), (150.00, 300.0), (450.0, 300.0)]);
+    draw_triangle(&context, [(150.0, 300.0), (0.0, 600.0), (300.0, 600.0)]);
+    draw_triangle(&context, [(450.0, 300.0), (300.0, 600.0), (600.0, 600.0)]);
 }
